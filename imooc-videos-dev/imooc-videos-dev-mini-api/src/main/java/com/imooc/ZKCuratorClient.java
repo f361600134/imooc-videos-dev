c8f74f038cdfad053dd15f35b69e48574051c90e
CHYME <CHYME@DESKTOP-HG5EAVU>
2018-09-30 10:49:36 +0800

    读取路径修改

--- imooc-videos-dev/imooc-videos-dev-mini-api/src/main/java/com/imooc/ZKCuratorClient.java
+++ imooc-videos-dev/imooc-videos-dev-mini-api/src/main/java/com/imooc/ZKCuratorClient.java
@@ -98,7 +98,8 @@ public class ZKCuratorClient {
 					String filePath = resourceConfig.getFileSpace() + songPath;
 					
 					// 3. 定义下载的路径（播放url）
-					String arrPath[] = songPath.split("\\\\");
+					//String arrPath[] = songPath.split("\\\\");
+					String arrPath[] = songPath.split("/");
 					String finalPath = "";
 					// 3.1 处理url的斜杠以及编码
 					for(int i = 0; i < arrPath.length ; i ++) {
